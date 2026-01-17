17b59c48088b67dc5d5f94ccf58e134175284952
MichaU <44945382+MichalThiede@users.noreply.github.com>
2026-01-17 17:26:28 +0100

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,125 @@
+import tseslint from "typescript-eslint";
+import angular from "@angular-eslint/eslint-plugin";
+import angularTemplate from "@angular-eslint/eslint-plugin-template";
+import angularTemplateParser from "@angular-eslint/template-parser";
+import prettier from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: [
+      "src/index.html", // czysty HTML (doctype)
+      "dist/**",
+      "coverage/**",
+      ".angular/**",
+    ],
+  },
+
+  // TypeScript ONLY
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "@angular-eslint": angular,
+      prettier,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...angular.configs.recommended.rules,
+      "prettier/prettier": "error",
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+          custom: {
+            regex: "^I[A-Z]",
+            match: true,
+          },
+        },
+        {
+          selector: "class",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "function",
+          format: ["camelCase"],
+        },
+        {
+          selector: "method",
+          format: ["camelCase"],
+        },
+        {
+          selector: "variable",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "property",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          modifiers: ["const"],
+          format: ["UPPER_CASE"],
+        },
+        {
+          selector: "objectLiteralProperty",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+      ],
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/no-empty-function": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-inferrable-types": "off",
+      "@typescript-eslint/typedef": [
+        "error",
+        {
+          memberVariableDeclaration: true,
+          propertyDeclaration: true,
+          parameter: true,
+          arrowParameter: true,
+        },
+      ],
+    },
+  },
+
+  // Angular templates
+  {
+    files: ["**/*.html"],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      "@angular-eslint/template": angularTemplate,
+    },
+    rules: {
+      ...angularTemplate.configs.recommended.rules,
+    },
+  },
+
+  // Disable formatting rules (Prettier wins)
+  eslintConfigPrettier,
+];
