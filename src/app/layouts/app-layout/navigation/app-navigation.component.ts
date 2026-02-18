d38b712669790687c5703e8e98533130301b8a12
MichaU <thiede92@gmail.com>
2026-02-18 18:09:35 +0100

    chore: clean up navigation component

--- src/app/layouts/app-layout/navigation/app-navigation.component.ts
+++ src/app/layouts/app-layout/navigation/app-navigation.component.ts
@@ -2,16 +2,13 @@ import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 
-import { firstValueFrom, Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { firstValueFrom } from 'rxjs';
 
 import { AuthService, ThemeService, HasFeatureDirective } from '@core';
 
@@ -38,7 +35,6 @@ import { MatMenuModule } from '@angular/material/menu';
   ],
 })
 export class AppNavigationComponent {
-  private breakpointObserver = inject(BreakpointObserver);
   private auth = inject(AuthService);
   private router = inject(Router);
   private nav = inject(AppNavigationService);
@@ -49,13 +45,6 @@ export class AppNavigationComponent {
   public constructor() {
     this.menuItems = this.nav.getMenu();
   }
-
-  public isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay(),
-    );
 
   public async logout(): Promise<void> {
     await firstValueFrom(this.auth.logout());
