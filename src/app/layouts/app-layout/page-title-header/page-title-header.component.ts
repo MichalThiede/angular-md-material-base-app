80e73862e15e633daa3840a1e52e3c3d3e7609e1
MichaU <thiede92@gmail.com>
2026-02-19 17:58:57 +0100

    refactor: improve page-title-header from rxjs to signal

--- src/app/layouts/app-layout/page-title-header/page-title-header.component.ts
+++ src/app/layouts/app-layout/page-title-header/page-title-header.component.ts
@@ -1,34 +1,31 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, startWith } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Component, computed, inject } from '@angular/core';
 
 @Component({
   selector: 'app-page-title-header',
-  template: `<div class="text-3xl pb-9">{{ pageTitle }}</div>`,
+  template: `<div class="text-3xl pb-9">{{ pageTitle() }}</div>`,
 })
-export class PageTitleHeaderComponent implements OnInit {
-  private route = inject(ActivatedRoute);
+export class PageTitleHeaderComponent {
   private router = inject(Router);
+  private route = inject(ActivatedRoute);
 
-  public pageTitle = '';
+  private navigationEnd = toSignal(
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd),
+      startWith(null),
+    ),
+  );
 
-  public ngOnInit(): void {
-    this.updateTitle();
+  public pageTitle = computed(() => {
+    this.navigationEnd(); // zależność reaktywna
 
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.updateTitle();
-      });
-  }
-
-  private updateTitle(): void {
     let activeRoute = this.route;
-
     while (activeRoute.firstChild) {
       activeRoute = activeRoute.firstChild;
     }
 
-    this.pageTitle = activeRoute.snapshot.data['pageTitle'] ?? '';
-  }
+    return activeRoute.snapshot.data['pageTitle'] ?? '';
+  });
 }
